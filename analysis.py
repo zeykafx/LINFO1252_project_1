bdc5f43e3494bacbb26034abfab039245e5845a8
Corentin <corentin.detry@gmail.com>
2022-11-23 12:27:24 +0100

    fixed graphs

--- analysis.py
+++ analysis.py
@@ -20,7 +20,7 @@ class CurrentProblem(Enum):
 
 
 def plot_file(file, current_problems: (str, str)):
-    old_problem, new_problem = current_problems
+    new_problem, old_problem = current_problems
 
     reader = csv.reader(file, delimiter=",")
 
@@ -28,8 +28,8 @@ def plot_file(file, current_problems: (str, str)):
     y = []
     y_err = []
 
-    new_problem_y = []
-    new_problem_y_err = []
+    old_problem_y = []
+    old_problem_y_err = []
 
     for row in reader:
         threads.append(row[0])
@@ -42,7 +42,7 @@ def plot_file(file, current_problems: (str, str)):
         y_err.append(stdev)
         y.append(statistics.mean(float_row))
 
-    with open(f"./data/{new_problem}.csv") as f2:
+    with open(f"./data/{old_problem}.csv") as f2:
         reader2 = csv.reader(f2, delimiter=",")
         for row2 in reader2:
             float_row2 = []
@@ -52,8 +52,8 @@ def plot_file(file, current_problems: (str, str)):
                 float_row2.append(duration)
 
             stdev = statistics.stdev(float_row2)
-            new_problem_y_err.append(stdev)
-            new_problem_y.append(statistics.mean(float_row2))
+            old_problem_y_err.append(stdev)
+            old_problem_y.append(statistics.mean(float_row2))
 
     width = 10
     height = 8
@@ -62,25 +62,25 @@ def plot_file(file, current_problems: (str, str)):
 
     plt.figure(figsize=(width, height))
 
-    plt.bar(X_axis, y, 0.4, label=old_problem)
+    plt.bar(X_axis, y, 0.4, label=new_problem)
     plt.errorbar(X_axis, y, y_err, fmt=".", color="Black", elinewidth=2, capthick=10, errorevery=1, alpha=0.5, ms=4,
                  capsize=2)
 
-    plt.bar(X_axis - 0.4, new_problem_y, 0.4, label=new_problem)
-    plt.errorbar(X_axis - 0.4, new_problem_y, new_problem_y_err, fmt=".", color="Black", elinewidth=2, capthick=10, errorevery=1,
+    plt.bar(X_axis - 0.4, old_problem_y, 0.4, label=old_problem.replace("old_", "") + " with pthread sync")
+    plt.errorbar(X_axis - 0.4, old_problem_y, old_problem_y_err, fmt=".", color="Black", elinewidth=2, capthick=10, errorevery=1,
                  alpha=0.5, ms=4,
                  capsize=2)
 
-    name = old_problem.replace("_", " ")
-    if old_problem == CurrentProblem.TEST_AND_SET_LOCK.value:
-        name = old_problem.replace("_", " ") + " & test and test and and set"
+    name = new_problem.replace("_", " ")
+    if new_problem == CurrentProblem.TEST_AND_SET_LOCK.value:
+        name = new_problem.replace("_", " ") + " & test and test and and set"
 
     plt.title(f"{name} problem: time taken for N threads, avg of 5 runs")
 
     plt.xlabel("Number of threads per run")
     plt.ylabel("Time taken in seconds for each run")
-
-    plt.savefig(f'./data/figure_{old_problem}.png', dpi=400, transparent=False)
+    plt.legend()
+    plt.savefig(f'./data/figure_{new_problem}.png', dpi=400, transparent=False)
     # plt.show()
 
 
